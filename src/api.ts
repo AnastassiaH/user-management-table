bed058b4d9ae6ad006781ce1e88fb9a0386fb39f
Anastasiia Hurska <nastyagurska@gmail.com>
2024-08-30 17:38:56 +0300

    store added

---
+++ src/api.ts
@@ -0,0 +1,9 @@
+import { User } from "./types/User";
+
+const API_URL = "https://jsonplaceholder.typicode.com/users";
+
+export async function getUsers(): Promise<User[]> {
+  return fetch(API_URL).then((response) => {
+    return response.json();
+  });
+}
---
+++ src/features/users.ts
@@ -0,0 +1,40 @@
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { User } from "../types/User";
+import { getUsers } from "../api";
+
+type UserState = {
+  users: User[];
+  isUsersLoading: boolean;
+  usersError: string;
+};
+
+const initialState: UserState = {
+  users: [],
+  isUsersLoading: false,
+  usersError: "",
+};
+
+export const init = createAsyncThunk("users/fetch", () => {
+  return getUsers();
+});
+
+export const userSlice = createSlice({
+  name: "users",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(init.pending, (state) => {
+      state.isUsersLoading = true;
+    });
+    builder.addCase(init.fulfilled, (state, action: PayloadAction<User[]>) => {
+      state.isUsersLoading = false;
+      state.users = action.payload;
+    });
+    builder.addCase(init.rejected, (state) => {
+      state.isUsersLoading = false;
+      state.usersError = "Something went wrong";
+    });
+  },
+});
+
+export default userSlice.reducer;
